ddf2ddff1587946211c25d50dfaef90cea754c19
annadorer <78897263+annadorer@users.noreply.github.com>
2023-01-24 14:44:36 +0700

    Add files via upload

---
+++ func.swift
@@ -0,0 +1,12 @@
+//
+//  func.swift
+//  TestProject
+//
+//  Created by Anna on 24.01.2023.
+//
+
+import Foundation
+
+func sum(_ a: String?, _ b: String?) -> Int {
+    return Int(a!)! + Int(b!)!
+}
---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  TestProject
+//
+//  Created by Anna on 24.01.2023.
+//
+
+print("Введите значение первого аргумента")
+var a = readLine()
+print("Введите значение второго аргумента")
+var b = readLine()
+
+let result = sum(a, b)
+print("Результат сложения - \(result)")
